4ea56ee870ebab9c1a7fabc34d031a6b67db86c1
danradom <danradom@users.noreply.github.com>
2014-09-25 21:16:02 -0600

    hue.sh
    bash script to control Philips Hue lights.  Supports groups, off, on (low, medium, high), party mode

---
+++ hue.sh
@@ -0,0 +1,108 @@
+# define input order
+group=$1
+state=$2
+intensity=$3
+
+
+# define groups
+living="1 2 3 4"
+bed="5"
+all="1 2 3 4 5"
+
+
+# validate input
+if [ $# -lt 2 ]; then
+	echo ""
+	echo "usage:  $0 <group> (all|living|bed) <state> (on|off|party)"
+	echo ""
+	exit 1
+fi
+
+if [ $2 = "on" ]; then
+	if [ $# -lt 3 ]; then
+		echo ""
+		echo "usage:  $0 $1 $2 <intensity> (low|medium|high)"
+		echo ""
+		exit 1
+	fi
+fi
+
+
+# traphandler function to turn off party mode
+traphandler () {
+	echo ""
+	echo "	turning off party mode"
+        for light in $lights; do
+                curl -X PUT -d '{"on":false,"effect":"none"}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+		usleep 100000
+        done
+	echo ""
+        exit 1234
+}
+trap traphandler SIGHUP SIGINT SIGTERM
+
+
+# define lights
+if [ $group = "living" ]; then
+	lights="$living"
+elif [ $group = "bed" ]; then
+	lights="$bed"
+elif [ $group = "all" ]; then
+	lights="$all"
+fi
+
+
+# define connection hash and bridge IP
+hash="3f2c333c592c232c4e2c5340532c4334"
+bridge="192.168.0.209"
+
+
+# light on function
+lights_on () {
+	if [ $intensity = "low" ]; then
+		for light in $lights; do
+			curl -X PUT -d '{"on":true,"ct":500}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+		done
+	elif [ $intensity = "medium" ]; then
+		for light in $lights; do
+			curl -X PUT -d '{"on":true,"ct":300}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+		done
+	elif [ $intensity = "high" ]; then
+		for light in $lights; do
+			curl -X PUT -d '{"on":true,"ct":154}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+		done
+	fi
+}
+
+
+# light off function
+lights_off () {
+	for light in $lights; do
+		curl -X PUT -d '{"on":false}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+	done
+}
+
+
+# party function
+lights_party () {
+	while true; do
+		for light in $lights; do
+			bri=`shuf -i 0-255 -n 1`
+			hue=`shuf -i 0-65535 -n 1`
+			sat=`shuf -i 0-255 -n 1`
+			curl -X PUT -d '{"on":true,"bri":'$bri',"sat":'$sat',"hue":'$hue'}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+			# curl -X PUT -d '{"on":false}' http://$bridge/api/$hash/lights/$light/state > /dev/null 2>&1
+			usleep 100000
+		done
+	done
+}
+
+
+# turn lights on and off
+if [ $2 = "on" ]; then
+	lights_on
+elif [ $2 = "off" ]; then
+	lights_off
+elif [ $2 = "party" ]; then
+	lights_party
+fi
